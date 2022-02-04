fb98d2aed50310b23803d70aa32a6df74c74ec0d
Tony <tonycarpenter@gmail.com>
2022-02-04 14:33:38 -0500

    Finish styling homepage and modals

--- src/MovieModal.js
+++ src/MovieModal.js
@@ -3,15 +3,25 @@ import "./MovieModal.css";
 
 const MovieModal = ({ movie }) => {
   return (
-    <section className="movie-preview-card">
+    <section className="movie-modal-preview-card">
       <img
-        className="movie-card-image"
+        className="movie-modal-card-image"
         src={movie.backdrop_path}
         alt={movie.title}
       />
       <h2>{movie.title}</h2>
-      <h3>Avg. Rating: {movie.average_rating.toFixed(1)} </h3>
-      <h4>Year of Release: {movie.release_date.substring(0, 4)}</h4>
+      <div className="movie-modal-information">
+
+        <div className="left-side">
+          <h3>Runtime: TIME GOES HERE </h3>
+          <h3>Release: {movie.release_date.substring(0, 4)}</h3>
+          <h3>Description: SOMETHING GOES HERE</h3>
+        </div>
+        <div className="right-side">
+          <h3>Rating: {movie.average_rating.toFixed(1)} </h3>
+          <h3>Genre: GENRE GOES HERE </h3>
+        </div>
+      </div>
     </section>
   );
 };
