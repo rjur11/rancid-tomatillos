50d7638f1cc2f9c49e99d04f310673a38a910267
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-02-05 16:02:33 -0500

    Add single movie api call and logic

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,17 @@
 const getAllMovies = async () => {
-  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-  .then(response => response.json())
-}
+  return fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies").then(
+    (response) =>
+      response.json().then((data) => {
+        console.log(data);
+        return data;
+      })
+  );
+};
 
-export default getAllMovies;
+const getSingleMovie = async (id) => {
+  return fetch(
+    `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`
+  ).then((response) => response.json());
+};
+
+export { getAllMovies, getSingleMovie };
