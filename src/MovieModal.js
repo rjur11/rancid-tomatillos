a5ecf3e16138f9b127ae45399a231c7434d4336e
Tony <tonycarpenter@gmail.com>
2022-02-08 17:47:53 -0500

    Adjust movie modal description layout, font styles and optimize for mobile

--- src/MovieModal.js
+++ src/MovieModal.js
@@ -47,17 +47,17 @@ class MovieModal extends Component {
         <div className="movie-modal-information">
           <div className="left-side">
             <h3 className="runtime-value">
-              Runtime:{" "}
+              Runtime:{" "}<span className="section-info">
               {this.state.fullMovie.runtime
                 ? `${this.state.fullMovie.runtime} minutes`
-                : "Information not available"}
+                : "Information not available"}</span>
             </h3>
-            <h3 className="release-date-value">Release: {this.state.movie.release_date.substring(0, 4)}</h3>
+            <h3 className="release-date-value">Release: <span className="section-info">{this.state.movie.release_date.substring(0, 4)}</span></h3>
             <h3 className="description-value">
-              Description:{" "}
+              Description:{" "}<span className="section-info">
               {this.state.fullMovie.overview
                 ? this.state.fullMovie.overview
-                : "Information not available"}
+                : "Information not available"}</span>
             </h3>
           </div>
           <div className="right-side">
@@ -70,10 +70,10 @@ class MovieModal extends Component {
               />{" "}
             </h3>
             <h3 className="genre-value">
-              Genre:{" "}
+              Genre:{" "}<span className="section-info">
               {this.state.fullMovie.genres.name
                 ? this.state.fullMovie.genres.join(", ")
-                : "Information not available"}
+                : "Information not available"}</span>
             </h3>
           </div>
         </div>
