0cb06626d8cf7519ff5af27c8104e4c5d502c49a
Tony <tonycarpenter@gmail.com>
2022-02-08 22:45:52 -0500

    Complete accessibility audit for lighthouse and WAVE

--- src/RatingDisplay.js
+++ src/RatingDisplay.js
@@ -6,10 +6,10 @@ export default function RatingDisplay({ rating }) {
   return (
     <div className="rating">
       {[...Array(rating)].map(() => (
-        <img src={tomatillo} className="tomatillo" />
+        <img src={tomatillo} className="tomatillo" alt="tomatillo" />
       ))}
       {[...Array(5 - rating)].map(() => (
-        <img src={emptyTomatillo} className="empty-tomatillo" />
+        <img src={emptyTomatillo} className="empty-tomatillo" alt="empty-tomatillo" />
       ))}
     </div>
   );
