c72bcaa31a5d8a93e029c915ce28f2aeb1b6471f
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-02-11 10:58:54 -0500

    Add error message for searched title when not in movies data

--- src/MovieContainer.js
+++ src/MovieContainer.js
@@ -5,20 +5,24 @@ import MovieCard from "./MovieCard";
 const MovieContainer = ({ movieArray, movieClicked }) => {
   return (
     <div className="movie-container">
-      {movieArray.map((movie) => {
-        return (
-          <MovieCard
-            onClick={() => movieClicked(movie)}
-            key={movie.id}
-            id={movie.id}
-            poster_path={movie.poster_path}
-            backdrop_path={movie.backdrop_path}
-            title={movie.title}
-            average_rating={Math.ceil(movie.average_rating / 2)}
-            release_date={movie.release_date}
-          />
-        );
-      })}
+      {movieArray.length > 0 ? (
+        movieArray.map((movie) => {
+          return (
+            <MovieCard
+              onClick={() => movieClicked(movie)}
+              key={movie.id}
+              id={movie.id}
+              poster_path={movie.poster_path}
+              backdrop_path={movie.backdrop_path}
+              title={movie.title}
+              average_rating={Math.ceil(movie.average_rating / 2)}
+              release_date={movie.release_date}
+            />
+          );
+        })
+      ) : (
+        <p>No movie found with that title. Please search for another movie.</p>
+      )}
     </div>
   );
 };
