ff1b91ad4d3105ad6eb5701bb437ecd257917886
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-02-11 16:31:00 -0500

    Clean up old variables and update testing

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -2,11 +2,6 @@ describe("Load homepage and render the Navigation Bar elements", () => {
   it("Should be able to visit the page and render the correct elements", () => {
     cy.visit("http://localhost:3000").contains("h1", "RancidTomatillos");
   });
-  it("Should render the search bar", () => {
-    cy.get('input[type="text-box"]')
-      .type("Mulan")
-      .should("have.value", "Mulan");
-  });
   it("Should render movie images", () => {
     cy.get('img[alt="Money Plane"]').should("be.visible");
   });
@@ -16,39 +11,54 @@ describe("Load homepage and render the Navigation Bar elements", () => {
   it("Should render movie year", () => {
     cy.get("h2.release-date-value").should("be.visible");
   });
-  it('Should go to movie modal view when movie button is clicked', () => {
+  it("Should go to movie modal view when movie button is clicked", () => {
     cy.get('img[alt="Money Plane"')
       .click()
       .url()
-      .should('eq', 'http://localhost:3000/694919')
+      .should("eq", "http://localhost:3000/694919");
   });
-  it('Should be able to click the back button in the browser to go home', () => {
+  it("Should be able to click the back button in the browser to go home", () => {
     cy.visit("http://localhost:3000")
       .get('img[alt="Money Plane"')
       .click()
       .url()
-      .should('eq', 'http://localhost:3000/694919')
-      .go('back')
+      .should("eq", "http://localhost:3000/694919")
+      .go("back")
       .url()
-      .should('eq', 'http://localhost:3000/')
+      .should("eq", "http://localhost:3000/");
   });
-  it('Should be able to click the forward button in the browser to go home', () => {
+  it("Should be able to click the forward button in the browser to go home", () => {
     cy.visit("http://localhost:3000")
       .get('img[alt="Money Plane"')
       .click()
       .url()
-      .should('eq', 'http://localhost:3000/694919')
-      .go('back')
+      .should("eq", "http://localhost:3000/694919")
+      .go("back")
       .url()
-      .should('eq', 'http://localhost:3000/')
-      .go('forward')
+      .should("eq", "http://localhost:3000/")
+      .go("forward")
       .url()
-      .should('eq', 'http://localhost:3000/694919')
+      .should("eq", "http://localhost:3000/694919");
+  });
+  it("Should return movie search results", () => {
+    cy.visit("http://localhost:3000")
+      .get('input[type="text-box"]')
+      .type("Mulan")
+      .should("have.value", "Mulan")
+      .get('img[alt="Mulan"]')
+      .should("be.visible")
+      .get('img[alt="Money Plane"]')
+      .should("not.exist");
+  });
+  it("Should return an error message if searched movie is not found", () => {
+    cy.visit("http://localhost:3000")
+      .get('input[type="text-box"]')
+      .type("Mulane")
+      .should("have.value", "Mulane")
+      .get("p")
+      .contains(
+        "No movie found with that title. Please search for another movie."
+      )
+      .should("be.visible");
   });
 });
-
-// TEST SAD PATH EMPTY SEARCH BAR/MAKE ERROR MESSAGE
-// it('should display an error message when a user clicks the Submit button without filling both inputs', () => {
-//   cy.get('button').click()
-//   cy.contains('Please fill out both inputs')
-// });
--- cypress/integration/movieModal_spec.js
+++ cypress/integration/movieModal_spec.js
@@ -1,49 +1,45 @@
 describe("Load homepage and render the Navigation Bar elements", () => {
-  it('Should be able to visit the modal page and render the correct elements', () => {
-    cy.visit('http://localhost:3000/694919')
-    .contains('h2', 'Money Plane');
+  it("Should be able to visit the modal page and render the correct elements", () => {
+    cy.visit("http://localhost:3000/694919").contains("h2", "Money Plane");
   });
-  it('Should render a movie image', () => {
-    cy.get('img[alt="Money Plane"]')
-      .should('be.visible')
-    });
-  it('Should render movie runtime value', () => {
-    cy.visit('http://localhost:3000/694919')
-    .contains('h3.runtime-value', '82 minutes');
+  it("Should render a movie image", () => {
+    cy.get('img[alt="Money Plane"]').should("be.visible");
   });
-  it('Should render movie release date year value', () => {
-    cy.visit('http://localhost:3000/694919')
-    .contains('h3.release-date-value', '2020');
+  it("Should render movie runtime value", () => {
+    cy.visit("http://localhost:3000/694919").contains(
+      "h3.runtime-value",
+      "82 minutes"
+    );
   });
-  it('Should render movie description', () => {
-    cy.visit('http://localhost:3000/694919')
-    .contains('h3.description-value', "Description: A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.");
+  it("Should render movie release date year value", () => {
+    cy.visit("http://localhost:3000/694919").contains(
+      "h3.modal-release-date-value",
+      "2020"
+    );
   });
-  it('Should render movie ratings', () => {
-    cy.get('div.rating')
-      .find("img")
-      .should('be.visible')
-    });
-  it('Should render movie genres', () => {
-    cy.visit('http://localhost:3000/694919')
-    .contains('h3.genre-value', "Genre: Information not available");
+  it("Should render movie description", () => {
+    cy.visit("http://localhost:3000/694919").contains(
+      "h3.description-value",
+      "Description: A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals."
+    );
   });
-  it('Should should show an exit button', () => {
-    cy.visit('http://localhost:3000/694919')
-    cy.get('a.exit-modal')
-      .should('be.visible')
+  it("Should render movie ratings", () => {
+    cy.get("div.rating").find("img").should("be.visible");
   });
-  it('Should go back to homepage when exit button is clicked', () => {
-    cy.get('a.exit-modal')
-      .click()
-      .url()
-      .should('eq', 'http://localhost:3000/')
+  it("Should render movie genres", () => {
+    cy.visit("http://localhost:3000/694919").contains(
+      "h3.genre-value",
+      "Action"
+    );
   });
-
+  it("Should should show an exit button", () => {
+    cy.visit("http://localhost:3000/694919");
+    cy.get("a.exit-modal").should("be.visible");
+  });
+  it("Should go back to homepage when exit button is clicked", () => {
+    cy.get("a.exit-modal").click().url().should("eq", "http://localhost:3000/");
+  });
 });
-
-// className="exit-modal" ///check original test, project board
-
 
 // it('should be able to fill out the email and password and click Submit, directing the user to a different page', () => {
 //   cy.intercept('POST', 'http://localhost:3001/api/v1/login', {
